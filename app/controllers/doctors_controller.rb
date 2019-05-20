953ba82d5c350dc5f5dae176d5b2f4613fa4bd24
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-20 21:48:18 +0000

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,13 +1,9 @@
 class DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+  end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_doctor
-      @doctor = Doctor.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def doctor_params
-      params.require(:doctor).permit(:name)
-    end
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
 end
--- app/views/doctors/_doctor.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! doctor, :id, :name, :created_at, :updated_at
-json.url doctor_url(doctor, format: :json)
