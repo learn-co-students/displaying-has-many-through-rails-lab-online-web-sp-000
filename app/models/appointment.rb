930abf5d0e7b5539e8272df97e635ccc682bfe0c
tan-nate <nktan93@gmail.com>
2019-07-27 17:09:49 -0700

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
     belongs_to :doctor
     belongs_to :patient
+
+    def readable_appointment_time
+        self.appointment_time.strftime('%B %d, %Y') + " at " + self.appointment_time.strftime('%H:%M')
+    end
 end
