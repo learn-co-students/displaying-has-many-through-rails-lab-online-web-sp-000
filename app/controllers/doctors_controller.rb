ae278c85c56dcec5e3f89b3f69b534f3b8eddd8f
tan-nate <nktan93@gmail.com>
2019-07-27 17:01:15 -0700

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,16 @@
 class DoctorsController < ApplicationController
+    before_action :set_doctor, only: :show
+
+    def show
+    end
+
+    def index
+        @doctors = Doctor.all
+    end
+
+    private
+
+    def set_doctor
+        @doctor = Doctor.find(params[:id])
+    end
 end
