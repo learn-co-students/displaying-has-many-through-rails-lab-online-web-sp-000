7eb2923ffa3f8f6defb777eab1ae5c735a4faf4b
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-02 00:34:54 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'patients/index'
+
+  get 'patients/view'
+
+  get 'patients/show'
+
+  get 'doctor/view'
+
+  get 'doctor/show'
+
+  get 'appointment/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
