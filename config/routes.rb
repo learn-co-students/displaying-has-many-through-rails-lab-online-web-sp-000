c5d600ec7e9be7662cbeb9427cfddcb25328be35
Ramon Chavez <rachavez1974@gmail.com>
2019-08-20 17:17:25 -0400

    Add appointment show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'patients/show'
-
-  get 'patients/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors, only: [:show]
   resources :patients, only: [:index, :show]
