b9f39b3264b0a74d53992109966d07b50483bd32
Ramon Chavez <rachavez1974@gmail.com>
2019-08-20 15:39:37 -0400

    Add Appoinment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190820192838) do
+ActiveRecord::Schema.define(version: 20190820193524) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.date     "appointment_datetime"
+    t.integer  "doctor_id"
+    t.integer  "patient_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
