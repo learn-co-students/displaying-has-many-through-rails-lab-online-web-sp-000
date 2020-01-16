9b2742e9fd43714200816f0fcb873dc9a9e04bb2
Ronnie Stephens <rstephens@bluemorpho.cc>
2020-01-15 22:55:57 -0500

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,37 +1,20 @@
 class DoctorsController < ApplicationController
-  before_action :set_doctor, only: [:show]
 
   def index
     @doctors = Doctor.all
   end
 
-  def new
-    @doctor = Doctor.new
-  end
-
   def show
-
+    @doctor = Doctor.find(params[:id])
+      # @date = @appointment.appointment_datetime.split('T')[0]
+      # @date = Date.parse(@date).strftime('%B %d, %Y')
+      # @time = @appointment.appointment_datetime.split('T')[1]
+      # @time = Time.parse(@time).strftime('%H:%M')
   end
 
   def create
     doctor = Doctor.create(doctor_params)
     redirect_to doctors_path
-  end
-
-
-  private
-
-  def set_doctor
-    @doctor = Doctor.find(params[:id])
-  end
-
-  def doctor_params
-    params.require(:doctor).permit(
-        :name, :department,
-        appointments_attributes: [
-          :appointment_datetime
-        ]
-    )
   end
 
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,38 +1,11 @@
 class PatientsController < ApplicationController
-  before_action :set_patient, only: [:show]
 
   def index
     @patients = Patient.all
   end
 
-  def new
-    @patient = Patient.new
-  end
-
-  def create
-    patient = Patient.create(patient_params)
-    redirect_to patients_path
-  end
-
   def show
-
-  end
-
-
-
-  private
-
-  def set_patient
     @patient = Patient.find(params[:id])
-  end
-
-  def patient_params
-    params.require(:patient).permit(
-        :name, :age,
-        appointments_attributes: [
-          :appointment_datetime
-        ]
-    )
   end
 
 end
