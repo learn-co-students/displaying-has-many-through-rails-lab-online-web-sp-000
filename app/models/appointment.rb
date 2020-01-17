7a1b0675dfde37644bffd3cddebef542310a8142
Ronnie Stephens <rstephens@bluemorpho.cc>
2020-01-16 22:18:06 -0500

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
 
+  # WORKS!!!  DO NOT TOUCH!!!!!!!
+  def appt_datetime
+    appointment_datetime.try(:strftime, '%B %d, %Y at %H:%M')
+  end
 end
