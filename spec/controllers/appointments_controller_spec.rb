2a985153b0c2024afd26a28e6109efceba2fd8e8
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-09 07:47:36 -0400

    Done.

---
+++ spec/controllers/appointments_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+=begin
+RSpec.describe AppointmentsController, type: :controller do
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+=end
+#end
