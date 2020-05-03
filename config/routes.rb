9e5d6b8ba2a1b1146dcd24bb72e027a19e2688d9
mmusah <mmusah1@gmail.com>
2020-05-03 17:13:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'appointments/index'
+
+  get 'appointments/show'
+
+  get 'appointments/new'
+
+  get 'appointments/create'
+
+  get 'appointments/destroy'
+
+  get 'appointments/edit'
+
+  get 'appointments/update'
+
+  get 'patients/index'
+
+  get 'patients/show'
+
+  get 'patients/new'
+
+  get 'patients/create'
+
+  get 'patients/destroy'
+
+  get 'patients/edit'
+
+  get 'patients/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
