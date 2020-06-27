1833a5db73865ea08380603cfc1718211f7707ff
carrollm2 <carrollm2@gmail.com>
2020-06-27 12:17:04 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'patients/show'
+
+  get 'patients/index'
+
   get 'doctors/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
