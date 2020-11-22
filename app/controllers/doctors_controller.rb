962220056d074786b3e13f99f2681941b670d668
LostnAustin <chadlengefeld@gmail.com>
2020-11-22 00:30:42 -0600

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,20 @@
 class DoctorsController < ApplicationController
-    def show
-        @doctor = Doctor.find_by(params[:id])
+    before_action :set_doctor, only: [:show]
+    
+    def index
+        @doctors = Doctor.all
     end
+
+    def show
+        
+    end
+
+    private
+        def set_doctor
+            @doctor = Doctor.find(params[:id])
+        end
+
+        def doctor_params
+            params.require(:doctor).permit(:name, :department)
+        end
 end
