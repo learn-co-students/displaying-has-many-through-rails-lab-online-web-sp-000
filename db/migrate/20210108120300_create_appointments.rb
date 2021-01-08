59939773dedb5d5b97a86954e20b2edc9e08bad1
john million <jpmillion@comcast.net>
2021-01-08 08:41:31 -0500

    Done.

---
+++ db/migrate/20210108120300_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.datetime :appointment_datetime
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
