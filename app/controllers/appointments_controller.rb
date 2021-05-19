285da5e7ea6e8ad6aa43e93a3418dff85e0bbfa1
GoldenPavilion <rcorystine@gmail.com>
2021-05-19 13:20:01 -0400

    update for review

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,9 +1,5 @@
 class AppointmentsController < ApplicationController
     
-    def index
-        @appointment = Appointment.find(params[:id])
-    end
-
     def show
         @appointment = Appointment.find(params[:id])
     end
