efe3e607a2880947502f9f7816a63d0addbf1f49
Dobakito <mdobak44@icloud.com>
2021-06-10 20:09:55 -0500

    Done.

---
+++ db/migrate/20210610235232_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.datetime :appointment_datetime
+      t.string :doctor_id
+      t.string :patient_id
+      t.timestamps
+    end
+  end
+end
